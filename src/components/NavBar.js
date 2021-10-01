027ed534c35d5c20630b1afa31068edcdec3e3c8
AmokelaneB <u17323402@tuks.co.za>
2021-10-01 22:29:32 +0200

    made some changes

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -39,7 +39,7 @@ export default function NavBar() {
         </nav>
         <div className="inline-flex py-3 px-3 my-6">
           <SocialIcon
-            url="https://www.instagram.com/mecterris/"
+            url="https://www.twitter.com/AmoSupreme/"
             className="mr-4"
             target="_blank"
             fgColor="#fff"
