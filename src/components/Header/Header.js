4900d612d337a5c8f38c9b021f07b19800a03ab6
AmokelaneB <u17323402@tuks.co.za>
2022-03-21 00:42:15 +0200

    Fixed Links

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,45 +1,59 @@
 import Link from 'next/link';
 import React from 'react';
-import { AiFillGithub, AiFillTwitterCircle, AiFillLinkedin } from 'react-icons/ai';
+import {
+  AiFillGithub,
+  AiFillTwitterCircle,
+  AiFillLinkedin,
+} from 'react-icons/ai';
 import { DiCssdeck } from 'react-icons/di';
 
-import { Container, Div1, Div2, Div3, NavLink, SocialIcons } from './HeaderStyles';
+import {
+  Container,
+  Div1,
+  Div2,
+  Div3,
+  NavLink,
+  SocialIcons,
+} from './HeaderStyles';
 
 const Header = () => (
   <Container>
     <Div1>
-      <Link href="/">
-        <a style={{ display: "flex", alignItems: "center", color: "white" }}>
-          <DiCssdeck size="3rem" /> <span>Portfolio</span>
+      <Link href='/'>
+        <a style={{ display: 'flex', alignItems: 'center', color: 'white' }}>
+          <DiCssdeck size='3rem' /> <span>Portfolio</span>
         </a>
       </Link>
     </Div1>
     <Div2>
       <li>
-        <Link href="#projects">
+        <Link href='#projects'>
           <NavLink>Projects</NavLink>
         </Link>
       </li>
       <li>
-        <Link href="#tech">
+        <Link href='#tech'>
           <NavLink>Technologies</NavLink>
         </Link>
       </li>
       <li>
-        <Link href="#about">
+        <Link href='#about'>
           <NavLink>About</NavLink>
         </Link>
       </li>
     </Div2>
     <Div3>
-      <SocialIcons href="https://github.com/AmokelaneB">
-        <AiFillGithub size="3rem" />
+      <SocialIcons target='_blank' href='https://github.com/AmokelaneB'>
+        <AiFillGithub size='3rem' />
       </SocialIcons>
-      <SocialIcons href="https://www.linkedin.com/in/amokelane-baloyi-0b39111bb/">
-        <AiFillLinkedin size="3rem" />
+      <SocialIcons
+        target='_blank'
+        href='https://www.linkedin.com/in/amokelane-baloyi-0b39111bb/'
+      >
+        <AiFillLinkedin size='3rem' />
       </SocialIcons>
-      <SocialIcons href="https://twitter.com/AmoSupreme">
-        <AiFillTwitterCircle size="3rem" />
+      <SocialIcons target='_blank' href='https://twitter.com/AmoSupreme'>
+        <AiFillTwitterCircle size='3rem' />
       </SocialIcons>
     </Div3>
   </Container>
